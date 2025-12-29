bf883a0737faded8c37d5c820e93e28eb17f7466
Connor Grant <71059606+c6grant@users.noreply.github.com>
2025-12-29 16:50:38 -0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [],
   template: `
     <div class="container">
-      Fun things are here.
+      Hello Studaliaders! We are thinking of going to dinner after my Stout call
+      that starts at 5:15 and likely goes to 5:30ish. Does that work for you??
     </div>
   `,
-  styles: [`
-    .container {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 100vh;
-      font-family: Arial, sans-serif;
-      font-size: 2rem;
-    }
-  `],
+  styles: [
+    `
+      .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        font-family: Arial, sans-serif;
+        font-size: 2rem;
+      }
+    `,
+  ],
 })
 export class AppComponent {}
